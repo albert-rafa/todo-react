36812463463c17f124794824b0c0c7a0d0624409
Rafa Albert <albertc.rafaela@gmail.com>
2024-03-06 10:30:10 -0300

    Done basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import './App.css'
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold text-red-700'>Olá, mundo</h1>
+    <div className='bg-red-700 w-full h-full'>
+      <div className='bg-lime-800 w-full h-full max-w-lg mx-auto flex flex-col justify-start items-center'>
+        <h1 className='bg-blue-800 w-full mt-3 py-3 text-center text-3xl'>ToDo</h1>
+
+        <main className='bg-green-500 grow w-full py-3 px-5'>
+          <div className="border-2 border-amber-600">Item</div>
+          <div className="border-2 border-amber-600">Item</div>
+          <div className="border-2 border-amber-600">Item</div>
+          <div className="border-2 border-amber-600">Item</div>
+        </main>
+      </div>
+    </div>
   )
 }
 
