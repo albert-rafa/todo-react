8ceebb4cf0a764b9833f27828d1eac0a8ae1f53a
Rafa Albert <albertc.rafaela@gmail.com>
2024-03-13 13:04:27 -0300

    Done item layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ function App() {
       <div className='bg-lime-800 w-full h-full max-w-lg mx-auto flex flex-col justify-start items-center'>
         <h1 className='bg-blue-800 w-full mt-3 py-3 text-center text-3xl'>ToDo</h1>
 
-        <main className='bg-green-500 grow w-full py-3 px-5'>
-          <div className="border-2 border-amber-600">Item</div>
+        <main className='bg-green-500 grow w-full py-4 px-5 flex flex-col justify-start gap-3 '>
+          <div className="border-2 border-amber-600 flex flex-row gap-2">
+            <div>done</div>
+            <div className="grow">text</div>
+            <div>delete</div>
+          </div>
+
           <div className="border-2 border-amber-600">Item</div>
           <div className="border-2 border-amber-600">Item</div>
           <div className="border-2 border-amber-600">Item</div>
