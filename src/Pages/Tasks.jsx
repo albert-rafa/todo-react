01bf536605c348bad2b6a2b2c37cdb2000b67d67
Rafa Albert <albertc.rafaela@gmail.com>
2024-03-19 21:39:27 -0300

    feat: add space at the bottom of scroll

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -4,24 +4,30 @@ import { Plus } from "lucide-react";
 export default function Add({ tasks, toggleTask, deleteTask, addTask }) {
 
   return (
-    <div className="border-2 border-amber-700 w-full h-full mb-10 flex flex-col gap-3 relative overflow-x-scroll">
-      {tasks.map((task) => {
-        return (
-          <Item
-            key={task.id}
-            task={task}
-            handleToggleTask={() => toggleTask(task)}
-            handleDeleteTask={() => deleteTask(task)}
-          />
-        );
-      })}
+    <div className="w-full max-h-[90%] flex flex-col relative overflow-hidden">
+      <div className="flex flex-col gap-3 overflow-x-scroll">
+        {tasks.map((task) => {
+          return (
+            <Item
+              key={task.id}
+              task={task}
+              handleToggleTask={() => toggleTask(task)}
+              handleDeleteTask={() => deleteTask(task)}
+            />
+          );
+        })}
 
-      <button
-        onClick={addTask} 
-        className="fixed bottom-4 left-1/2 -translate-x-1/2 size-20 rounded-full flex justify-center items-center bg-sky-800"
-      >
-        <Plus className="size-3/4 stroke-sky-300" />
-      </button>
+        <div className="w-full min-h-24"/>
+      </div>
+      
+      <div className="fixed bottom-2 w-full max-w-md h-28 bg-gradient-to-t from-sky-300 to-transparent flex flex-col justify-end pb-3">
+        <button
+          onClick={addTask} 
+          className="mx-auto size-20 rounded-full flex justify-center items-center bg-sky-800"
+        >
+          <Plus className="size-3/4 stroke-sky-300" />
+        </button>
+      </div>
     </div>
   );
 }
